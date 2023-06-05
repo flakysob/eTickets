346be360bc14b5fa6951317b7d1d89cb9eb122df
flakysob <flakysob@gmail.com>
2023-06-05 13:59:38 +0300

    End of the Chapter 27

---
+++ Models/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Profile Picture URL")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set;}
+        [Display(Name = "Biography")]
+        public string Bio { get; set; }
+
+        //Realtionships
+
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
