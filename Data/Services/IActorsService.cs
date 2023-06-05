0749414d027c8a52e8b0584cafad17a298b8b540
flakysob <flakysob@gmail.com>
2023-06-05 14:04:04 +0300

    End of the Chapter 28

--- Data/Services/IActorsService.cs
+++ Data/Services/IActorsService.cs
@@ -1,6 +1,14 @@
-﻿namespace eTickets.Data.Services
+﻿using eTickets.Models;
+using System.Collections.Generic;
+
+namespace eTickets.Data.Services
 {
     public interface IActorsService
     {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
     }
 }
