eb8e61e890dd7394b8e5d7e4144170aeea010f7f
flakysob <flakysob@gmail.com>
2023-06-12 17:26:34 +0300

    End of the Chapter 35

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -18,7 +18,7 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
 
@@ -30,10 +30,48 @@ namespace eTickets.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL, Bio")]Actor actor)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
+                return View(actor);
+            }
+            await _service.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
 
+        //Get The Data With Id (Actors/Details/1)
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails =  await _service.GetByIdAsync(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(actorDetails);
             }
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, FullName, ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+
+            await _service.UpdateAsync(id,actor);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
